c4347622835078bfd100f01db50908c972f5cb1b
mcmuncher <dmpope1@hotmail.com>
2017-04-19 20:56:31 -0500

    Commit 1: Implemented my first JavaScript script, Uploaded initial CSS and HTML files
    -Added pomodoro-timer.js which counts down from a user-submitted length
    of time using plain JavaScript

---
+++ js/pomodoro-timer.js
@@ -0,0 +1,23 @@
+(function() {
+	function timerDisplay() {												   //Caluclates remaining number of minutes and seconds from 
+		var today, currentTime, timeLength, sec, min, output, demDiv, elTimer; //milliseconds, Converts integer to readable display
+		today = new Date();
+		elTimer = document.getElementById('countdown');
+		currentTime = today.getTime();
+		timeRemaining = timerLength - currentTime;
+		sec = Math.floor((timeRemaining / 1000) % 60);
+		min = Math.floor(timeRemaining / 1000 / 60);
+		output = min + ' : ' + sec;
+		elTimer.textContent = output;
+	}
+	var displayTimer = setInterval(timerDisplay, 1000); 					   //Continuously updates DOM by repeatedly calling timerDisplay()
+	var timerLength = (function() { 										   //Calculates the length of timer based off user's selection 
+		var elSelect, mins, today, currentTime, deadline;
+		today = new Date();
+		currentTime = today.getTime();
+		elSelect = document.getElementById('lengthofTime');
+		mins = elSelect.options[elSelect.selectedIndex].value;
+		deadline = currentTime + mins*60*1000;
+		return deadline
+	})();
+})();
